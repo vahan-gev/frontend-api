e1cf9d4c797a0c3aa25c12acd20c8dc5bce61c94
Vahan Gevorgyan <47473115+vahan-gev@users.noreply.github.com>
2024-08-10 17:48:54 -0700

    Add files via upload

--- src/app/components/PlayerCard/PlayerCard.js
+++ src/app/components/PlayerCard/PlayerCard.js
@@ -3,12 +3,14 @@ export default function PlayerCard({player}) {
         <div className="flex py-2 px-4 bg-[#232323] rounded-xl gap-2 text-white justify-between hover:shadow-2xl cursor-pointer">
             <div className="w-[120px] max-w-[120px] flex flex-col items-center justify-center">
                 <span className="text-nowrap">{player?.fn + " " + player?.ln}</span>
-                <span className="text-[4rem]">{player?.pos}</span>
             </div>
-            <div
-                className="inline-block w-[0.5px] self-stretch bg-white"></div>
-            <div className="w-[120px] max-w-[120px] flex items-center justify-center">
-                <span className="text-[4rem]">{player?.val.toFixed(1)}</span>
+            <div className="inline-block w-[0.5px] self-stretch bg-white"></div>
+            <div className="w-[50px] max-w-[50px] flex flex-col items-center justify-center">
+                <span className="text-lg">{player?.pos}</span>
+            </div>
+            <div className="inline-block w-[0.5px] self-stretch bg-white"></div>
+            <div className="w-[50px] max-w-[50px] flex items-center justify-center">
+                <span className="text-lg">{player?.val.toFixed(1)}</span>
             </div>
         </div>
     )
--- src/app/components/Statistics/Statistics.js
+++ src/app/components/Statistics/Statistics.js
@@ -28,23 +28,41 @@ export default function Statistics() {
         <div className="py-4 flex flex-col items-center gap-4">
             <h1 className="text-[2rem] font-black uppercase text-[#232323]">Top 3</h1>
             <div className="flex gap-8">
-                <div className="flex gap-4 flex-col">
-                    <h1 className="text-lg font-black uppercase text-[#232323]">Points</h1>
+                <div className="flex gap-4 flex-col bg-[#ececec] py-4 px-6 rounded-xl">
+                    <div className="flex items-center justify-center">
+                        <h1 className="text-lg font-black uppercase text-[#232323]">Points</h1>
+                    </div>
                     {getTopThree(playersByPoints)?.map((player, index) => (
                         <PlayerCard key={index} player={player}/>
                     ))}
+                    <div
+                        className="flex py-2 px-4 bg-[#fcb724] rounded-xl items-center justify-center text-white hover:shadow-2xl cursor-pointer">
+                        <p>Show All</p>
+                    </div>
                 </div>
-                <div className="flex gap-4 flex-col">
-                    <h1 className="text-lg font-black uppercase text-[#232323]">Rebounds</h1>
+                <div className="flex gap-4 flex-col bg-[#ececec] py-4 px-6 rounded-xl">
+                    <div className="flex items-center justify-center">
+                        <h1 className="text-lg font-black uppercase text-[#232323]">Rebounds</h1>
+                    </div>
                     {getTopThree(playersByRebounds)?.map((player, index) => (
                         <PlayerCard key={index} player={player}/>
                     ))}
+                    <div
+                        className="flex py-2 px-4 bg-[#fcb724] rounded-xl items-center justify-center text-white hover:shadow-2xl cursor-pointer">
+                        <p>Show All</p>
+                    </div>
                 </div>
-                <div className="flex gap-4 flex-col">
-                    <h1 className="text-lg font-black uppercase text-[#232323]">Three pointers</h1>
+                <div className="flex gap-4 flex-col bg-[#ececec] py-4 px-6 rounded-xl">
+                    <div className="flex items-center justify-center">
+                        <h1 className="text-lg font-black uppercase text-[#232323]">Three Pointers</h1>
+                    </div>
                     {getTopThree(playersByThreePointers)?.map((player, index) => (
                         <PlayerCard key={index} player={player}/>
                     ))}
+                    <div
+                        className="flex py-2 px-4 bg-[#fcb724] rounded-xl items-center justify-center text-white hover:shadow-2xl cursor-pointer">
+                        <p>Show All</p>
+                    </div>
                 </div>
             </div>
         </div>
